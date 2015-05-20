b20846e3d00f7f8589d786c2120eeb7351971733
zchen9 <zxcvbnm.pop@qq.com>
2015-05-20 14:11:24 +0800

    add data

--- task0003/zchen9/js/todo.data.js
+++ task0003/zchen9/js/todo.data.js
@@ -25,6 +25,17 @@ TaskDetail.prototype = {
 		title:"Welcome to ToDo",
 		date: new Date(2015,0,1),
 		content:"Nice to meet U !  欢迎使用 ToDo 应用 ~ (๑•̀ㅂ•́)و✧"
+				+"<br>"+"<br>"
+				+"<i class='fa fa-flag'></i>"+"[标题]：不超过10个汉字"
+				+"<br>"
+				+"<i class='fa fa-calendar'></i>"+"[时间]：格式为 [YYYY-MM-DD] ( 例：2015-01-01 )"
+				+"<br>"
+				+"<i class='fa fa-pencil-square-o'></i>"+"[编辑]：可以编辑任务标题、时间、内容"
+				+"<br>"
+				+"<i class='fa fa-undo'></i>"+"[重置]：编辑状态时，点击可以重置任务"
+				+"<br>"
+				+"<i class='fa fa-check-square-o'></i>"+"[完成]：编辑状态时，点击为完成编辑；非编辑状态时，点击为完成任务"
+
 	}
 };
 
